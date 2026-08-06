f6d2bdcc19e6360960767aecab9d228f58694d40
Jonatha Gomes <contato.jonathagomesdev@gmail.com>
2026-08-05 23:47:44 -0300

    chore(package): add dev script using node --watch

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import http from "node:http"
 
 const server = http.createServer((request, response) => {
-  return response.end("Hello World");
+  return response.end("Hello John");
 });
 
 server.listen(3333);
