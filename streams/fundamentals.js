29da2d8f85c93d92a164c877747187471839c1f3
Jonatha Gomes <contato.jonathagomesdev@gmail.com>
2026-08-09 08:42:51 -0300

    feat: create transform stream to negate numbers

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -1,4 +1,4 @@
-import { Readable, Writable } from "node:stream"
+import { Readable, Writable, Transform } from "node:stream"
 
 class OneToHundredStream extends Readable {
   index = 1
@@ -16,6 +16,14 @@ class OneToHundredStream extends Readable {
   }
 }
 
+class InverseNumberStream extends Transform {
+  _transform(chunk, enconding, callback) {
+    const transformed = Number(chunk.toString()) * -1
+
+    callback(null, Buffer.from(String(transformed)))
+  }
+}
+
 class MultiplyByTenStream extends Writable {
   _write(chunk, enconding, callback) {
     console.log(Number(chunk.toString()) * 10)
@@ -23,4 +31,6 @@ class MultiplyByTenStream extends Writable {
   }
 }
 
-new OneToHundredStream().pipe(new MultiplyByTenStream())
+new OneToHundredStream()
+  .pipe(new InverseNumberStream())
+  .pipe(new MultiplyByTenStream())
